01d39f02781fcb4cf8ac948bd584efabb0978195
Lyudmila <Romanova>
2023-04-25 18:41:29 +0500

    init commit

---
+++ DotnetQuestion/DotnetQuestion/Example/Ex1(GC).cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetQuestion.Example
+{
+    internal class Ex1_GC_
+    {
+        public static void Task1()
+        {
+            var sum = 0;
+            for (var i = 0; i <= 10; i++;){
+                var j = i * 2;
+                sum += i;
+            }
+            Console.WriteLine(j);
+            Console.WriteLine(sum);
+        }
+    }
+}
